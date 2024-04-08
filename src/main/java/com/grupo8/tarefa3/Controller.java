0785a8efb3510ad28d2fac6030a7078cbd3d6737
Pedro Park <pedro.park@ael.com.br>
2024-04-08 18:22:51 -0300

    Ambiente pronto com uma pagina string funcional

---
+++ src/main/java/com/grupo8/tarefa3/Controller.java
@@ -0,0 +1,16 @@
+package com.grupo8.tarefa3;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("")
+    @CrossOrigin(origins = "*")
+    public String mensagemInicial(){
+        return "Hello World";
+    }
+
+}
---
+++ src/main/java/com/grupo8/tarefa3/Tarefa3Application.java
@@ -0,0 +1,13 @@
+package com.grupo8.tarefa3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarefa3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarefa3Application.class, args);
+	}
+
+}
